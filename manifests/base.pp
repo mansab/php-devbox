8c3ee397fdd9be94102b1edc77dc600dac941a28
Mansab Uppal <mansab.uppal@enadco.ag>
2015-03-25 18:20:46 +0100

    Updated the base.pp to include puppet and ssh

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,7 +1,19 @@
 class base(
   $common_packages = ['tmux', 'curl', 'wget', 'rsync', 'unzip', 'htop'],
-  $update_packages = {}
+  $update_packages = {},
+  $puppetmaster    = undef,
+  $puppetrunmode   = "none"
 ){
+   class { "ssh":
+     server_options => hiera('ssh_server_options'),
+     client_options => hiera('ssh_client_options')
+   }
+
+   class { "puppet-agent":
+     puppetmaster => $puppetmaster,
+     runmode      => $puppetrunmode
+   }
+
    ensure_packages($common_packages)
    create_resources('Package', $update_packages, {'ensure' => 'latest'})
 }
